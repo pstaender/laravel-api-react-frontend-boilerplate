b03e7d066fe126b5d60c28a7369fae7aaa3ba056
Philipp Staender <pstaender@mailbox.org>
2024-06-08 10:46:10 +0200

    refactoring routes, design

--- api/routes/api.php
+++ api/routes/api.php
@@ -20,108 +20,13 @@ use App\Http\Resources\UserResource;
 */
 
 Route::group(['prefix' => 'v1'], function () {
+
+    require_once __DIR__ . '/signup_routes.php';
+    require_once __DIR__ . '/forgot_password_routes.php';
+    require_once __DIR__ . '/logout_routes.php';
+    require_once __DIR__ . '/delete_user_account_routes.php';
+
     Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
         return $request->user();
-    });
-
-    Route::middleware('auth:sanctum')
-        ->post('/logout', function (Request $request) {
-            $request->user()->currentAccessToken()->delete();
-        });
-
-    Route::post('/signup', function (Request $request) {
-
-        $request->validate([
-            'email' => 'email|required',
-            'password' => 'required|min:8',
-        ]);
-
-        $email = strtolower($request->input('email'));
-
-        if (User::where(['email' => $email])->first()) {
-            return response()->json([
-                'error' => __('E-Mail already exists'),
-                'statusCode' => 409
-            ], 409);
-        }
-
-        if ($request->input('password') === $request->input('email')) {
-            return response()->json([
-                'error' => __('Password must be different than email'),
-                'statusCode' => 409
-            ], 409);
-        }
-        $user = User::create([
-            'email' => $email,
-            'password' => Hash::make($request->input('password')),
-            'signup_device' => $request->userAgent(),
-            'signup_ip' => $request->ip(),
-        ]);
-        $user->save();
-        // https://laravel.com/docs/10.x/verification
-        event(new \Illuminate\Auth\Events\Registered($user));
-
-        return [
-            'message' => __('Signup created. Please confirm your e-mail before logging in'),
-        ];
-    });
-
-    Route::get(
-        '/signup/email_is_available/{email}',
-        function ($email, Request $request): array {
-            sleep(.5);
-            $email = strtolower($email);
-            $valid = (new EmailValidator())->isValid($email, new \Egulias\EmailValidator\Validation\RFCValidation());
-            if (!$valid) {
-                return [
-                    'email_is_available' => false,
-                    'email_is_valid' => false,
-                ];
-            };
-            return [
-                'email_is_available' => !(bool)User::where(['email' => $email])->first(),
-                'email_is_valid' => true,
-            ];
-        }
-    );
-
-    Route::post('/resend_email_verification/{email}', function ($email) {
-        User::where(['email' => strtolower($email)])->first()->sendEmailVerificationNotification();
-    });
-
-    Route::middleware('auth:sanctum')
-        ->delete('/account', function (Request $request) {
-            $user = $request->user();
-            if (!\Illuminate\Support\Facades\Hash::check($request->input('password'), $user->password)) {
-                return response()->json([
-                    'error' => __('The password did not match'),
-                    'statusCode' => 409
-                ], 409);
-            }
-            \Illuminate\Support\Facades\DB::transaction(function () use ($user) {
-                $user->personalAccessTokens()->delete();
-                $user->delete();
-            });
-
-            return [
-                'status' => 'deleted'
-            ];
-        });
-
-
-    Route::post('/forgot-password', function (Request $request) {
-        $request->validate(['email' => 'required|email']);
-        $status = Password::sendResetLink(
-            $request->only('email')
-        );
-        return [
-            'status' => $status,
-        ];
-    })->name('password.email');
-
-    Route::get('/healthz', function (Request $request) {
-        // check that db is available
-        User::count();
-        return ['status' => 'ok'];
     });
 });
