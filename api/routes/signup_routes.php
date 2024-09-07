3ff9c73270119ace1c4fe08ff6d3e13233e349a2
Philipp Staender <pstaender@mailbox.org>
2024-09-07 08:32:02 +0200

    wording

--- api/routes/signup_routes.php
+++ api/routes/signup_routes.php
@@ -6,9 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Hash;
 
-
 Route::post('/signup', function (Request $request) {
-
     $request->validate([
         'email' => 'email|required',
         'password' => 'required|min:8',
@@ -17,17 +15,23 @@ Route::post('/signup', function (Request $request) {
     $email = strtolower($request->input('email'));
 
     if (User::where(['email' => $email])->first()) {
-        return response()->json([
-            'error' => __('E-Mail already exists'),
-            'statusCode' => 409
-        ], 409);
+        return response()->json(
+            [
+                'error' => __('E-Mail already exists'),
+                'statusCode' => 409,
+            ],
+            409
+        );
     }
 
     if ($request->input('password') === $request->input('email')) {
-        return response()->json([
-            'error' => __('Password must be different than email'),
-            'statusCode' => 409
-        ], 409);
+        return response()->json(
+            [
+                'error' => __('Password must be different than email'),
+                'statusCode' => 409,
+            ],
+            409
+        );
     }
     $user = User::create([
         'email' => $email,
@@ -42,29 +46,38 @@ Route::post('/signup', function (Request $request) {
     event(new \Illuminate\Auth\Events\Registered($user));
 
     return [
-        'message' => __('Signup created. Please confirm your e-mail before logging in'),
+        'message' => __(
+            'Account created. Please confirm your e-mail before logging in'
+        ),
     ];
 });
 
-Route::get(
-    '/signup/email_is_available/{email}',
-    function ($email, Request $request): array {
-        sleep(.5);
-        $email = strtolower($email);
-        $valid = (new EmailValidator())->isValid($email, new \Egulias\EmailValidator\Validation\RFCValidation());
-        if (!$valid) {
-            return [
-                'email_is_available' => false,
-                'email_is_valid' => false,
-            ];
-        };
+Route::get('/signup/email_is_available/{email}', function (
+    $email,
+    Request $request
+): array {
+    sleep(0.5);
+    $email = strtolower($email);
+    $valid = (new EmailValidator())->isValid(
+        $email,
+        new \Egulias\EmailValidator\Validation\RFCValidation()
+    );
+    if (!$valid) {
         return [
-            'email_is_available' => !(bool)User::where(['email' => $email])->first(),
-            'email_is_valid' => true,
+            'email_is_available' => false,
+            'email_is_valid' => false,
         ];
     }
-);
+    return [
+        'email_is_available' => !(bool) User::where([
+            'email' => $email,
+        ])->first(),
+        'email_is_valid' => true,
+    ];
+});
 
 Route::post('/resend_email_verification/{email}', function ($email) {
-    User::where(['email' => strtolower($email)])->first()->sendEmailVerificationNotification();
+    User::where(['email' => strtolower($email)])
+        ->first()
+        ->sendEmailVerificationNotification();
 });
